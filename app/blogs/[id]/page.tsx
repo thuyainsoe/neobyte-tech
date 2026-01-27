eee0c4c00068c2485a6d835405010ccb3bfa9fbf
thuyainsoe <thuyainsoe163361@gmail.com>
2026-01-27 12:51:52 +0700

    fix

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -1,7 +1,8 @@
 import BlogDetail from "@/components/BlogDetail";
 import React from "react";
 
-const BlogDetailPage = ({ params }: { params: { id: string } }) => {
+const BlogDetailPage = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
   return <BlogDetail />;
 };
 
