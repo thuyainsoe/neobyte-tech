3eec5509f80e17581425761c29d675dc7260a807
thuyainsoe <thuyainsoe163361@gmail.com>
2026-01-28 00:18:39 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
+  async rewrites() {
+    return [
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
+        source: "/api/:path*", // Frontend က /api/xxx လို့ခေါ်ရင်
+        destination: "http://143.198.197.70:1338/api/:path*", // Strapi ဆီ လှမ်းပို့မယ်
       },
-    ],
+      {
+        source: "/uploads/:path*", // ပုံတွေအတွက်
+        destination: "http://143.198.197.70:1338/uploads/:path*",
+      },
+    ];
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
